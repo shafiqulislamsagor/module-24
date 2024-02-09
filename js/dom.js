a02109604c4e93ebc2b66217ab60b9fa7ca2fe9f
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-09 23:25:24 +0600

    module 24-2

--- js/dom.js
+++ js/dom.js
@@ -1,2 +1,10 @@
 console.log(' helo');
-console.log(document)
+console.log(document);
+
+const allLiTags = document.getElementsByTagName('li')
+
+for(let li of allLiTags){
+    console.log(li)
+}
+
+console.log(allLiTags)
