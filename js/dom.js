e14fe754a5dcca9891b941263914ef1b3514e37c
shafiqulislamsagor <shafiqulislamsagor277@gmail.com>
2024-02-10 07:07:46 +0600

    dom related

--- js/dom.js
+++ js/dom.js
@@ -8,3 +8,20 @@ for(let li of allLiTags){
 }
 
 console.log(allLiTags)
+
+let rootId = document.getElementById('root').innerHTML = 'thes';
+
+
+
+console.log(rootId);
+
+
+// Inner Text
+
+const div = document.getElementById('my-details').innerHTML
+const divs = document.getElementById('my-details').innerText
+
+
+
+console.log(div)
+console.log(divs)
